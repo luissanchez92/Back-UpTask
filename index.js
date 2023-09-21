b95782701fb083ed1ade0033646798456190f78e
luissanchez92 <luisjosanchez69@gmail.com>
2023-09-21 10:55:59 -0300

    socket.io

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config()
 
 connectDB()
 //config cors
-const whiteList=[process.env.FRONT_URL_I, process.env.FRONT_URL_L];
+const whiteList=[process.env.FRONT_URL_I];
 
 const corsOptions={
     origin: function(origin, callback){
@@ -40,6 +40,48 @@ app.use('/api/task', taskRouter)
 
 const PORT= process.env.PORT || 3001
 
-app.listen(PORT, ()=>{
+const serverExpress=app.listen(PORT, ()=>{
     console.log(`server listening on port ${PORT}`)
+})
+
+
+//socket.Io
+import { Server, Socket } from "socket.io";
+
+const io= new Server(serverExpress, {
+    pingTimeout: 60000,
+    cors:{
+        origin: process.env.FRONT_URL_I
+    }
+})
+
+io.on('connection', (Socket)=>{
+    console.log('connect a socket.io')
+    // definir eventos
+    Socket.on('open project',(project)=>{
+        Socket.join(project)
+    })
+
+    Socket.on('newTask', (data)=>{
+        const project=data.project
+
+        if (typeof project==='string'){
+            Socket.on(project).emit('addedTask', task)
+        }else if(typeof project==='object'){
+            Socket.on(project._id).emit('addedTask', task)
+        }
+    })
+
+
+    Socket.on('deleteTask', (task)=>{
+        const projectValue = task.project
+
+        if (typeof projectValue === 'string') {
+            Socket.to(projectValue).emit('taskDeleted', task)
+        } else if (typeof projectValue === 'object') {
+            Socket.to(projectValue._id).emit('taskDeleted', task)
+        }
+    })
+
+
 })
